79da8676d06911084ccf6375d133965632c1385f
shad7423 <shadabkhanrps@gmail.com>
2018-04-11 12:25:37 +0530

    custom cell created

--- meditation-test/ViewController.swift
+++ meditation-test/ViewController.swift
@@ -8,34 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//struct cellData {
+//    let text: String!
+//    let image : UIImage!
+//    
+//    
+//}
 
+class ViewController: UIViewController ,UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    
+    
+    
+
+  //  var arrayCustomCell = [cellData]()
+    
+    var arrayImage = [UIImage(named: "albumArt"),UIImage(named: "albumArt"),UIImage(named: "albumArt"),UIImage(named: "albumArt")]
+    var arrayText = ["frst","second","third","fourth"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func popUp(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Today,learn about", message: "Choose from below topics", preferredStyle: UIAlertControllerStyle.alert)
-        let alertAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default)
-        let alertAction2 = UIAlertAction(title: "Select", style: UIAlertActionStyle.default){
-            (UIAlertAction) -> Void in
-            
-            
-        }
+       
         
-        alert.addAction(alertAction)
-        alert.addAction(alertAction2)
-        present(alert, animated: true)
-        {
-            () -> Void in
-        }
+       
+        
+        
     }
     
+    
+    
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return arrayImage.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! belowCollectionViewCell
+                cell.imageCustom.image = arrayImage[indexPath.row]
+                cell.textCustom.text = arrayText[indexPath.row]
+            
+            
+            return cell
+        }
+        
+        
+        
+    
+
+   
 }
 
---
+++ meditation-test/belowCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  belowCollectionViewCell.swift
+//  meditation-test
+//
+//  Created by Shadab Khan on 4/9/18.
+//  Copyright © 2018 Shadab Khan. All rights reserved.
+//
+
+import UIKit
+
+class belowCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageCustom: UIImageView!
+    
+   @IBOutlet weak var textCustom: UILabel!
+}
